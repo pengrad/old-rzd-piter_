2f29af432206600c21cc8302786c98648cf05639
s.a.parshin@gmail.com <s.a.parshin@gmail.com@9a6718c1-c977-9a1b-d0af-c96b8d1c4d0a>
2012-02-25 20:02:30 +0000

    [db]
    Исправлен запрос получения суммы по билетам. Округление - round(1)

--- trunk/src/db/queryMoney.sql
+++ trunk/src/db/queryMoney.sql
@@ -1,4 +1,4 @@
-select 'zd' type, ifnull(sum(t1.S),0) sum, ifnull(sum(t1.S),0) sumPay, null sumFedSoc, null sumFedNonSoc, null sumRegion, null sumWar, null sumStudy, null sumRZDPersonal, null sumRZDWork, null sumRZDService, 0 sumService
+select 'zd' type, ifnull(round(sum(t1.S),1),0) sum, ifnull(round(sum(t1.S),1),0) sumPay, null sumFedSoc, null sumFedNonSoc, null sumRegion, null sumWar, null sumStudy, null sumRZDPersonal, null sumRZDWork, null sumRZDService, 0 sumService
 from ticket t1
 	join file t2 on t1.FileId=t2.FileId
 where t1.TimeCalcReport >= '20010101' and t1.TimeCalcReport < '20130101'
@@ -7,14 +7,14 @@ where t1.TimeCalcReport >= '20010101' and t1.TimeCalcReport < '20130101'
 	and ifnull(t1.a,0) <> 1
 union all	
 
-select 'zn' type, ifnull(sum(t1.S),0) sum, null sumPay, 
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_fedsoc) then t1.S else 0 end),0) sumFedSoc, 
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_fednonsoc) then t1.S else 0 end),0) sumFedNonSoc, 
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_region) then t1.S else 0 end),0) sumRegion, 
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_war) then t1.S else 0 end),0) sumWar,
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_study) then t1.S else 0 end),0) sumStudy,
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_rzdpersonal) then t1.S else 0 end),0) sumRZDPersonal,
-	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_rzdwork) then t1.S else 0 end),0) sumRZDWork,	
+select 'zn' type, ifnull(sum(round(t1.S,1)),0) sum, null sumPay, 
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_fedsoc) then round(t1.S,1) else 0 end),0) sumFedSoc, 
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_fednonsoc) then round(t1.S,1) else 0 end),0) sumFedNonSoc, 
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_region) then round(t1.S,1) else 0 end),0) sumRegion, 
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_war) then round(t1.S,1) else 0 end),0) sumWar,
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_study) then round(t1.S,1) else 0 end),0) sumStudy,
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_rzdpersonal) then round(t1.S,1) else 0 end),0) sumRZDPersonal,
+	ifnull(sum(case when t1.TicketTypeL in (select disc_id from discount_rzdwork) then round(t1.S,1) else 0 end),0) sumRZDWork,	
 	null sumRZDService, null sumService
 from ticket t1
 	join file t2 on t1.FileId=t2.FileId
@@ -26,8 +26,8 @@ where t1.TimeCalcReport >= '20010101' and t1.TimeCalcReport < '20130101'
 	
 union all	
 
-select 'zp' type, ifnull(sum(t1.S),0) sum, null sumPay, null sumFedSoc, null sumFedNonSoc, null sumRegion, null sumWar, null sumStudy, null sumRZDPersonal, null sumRZDWork, 
-	ifnull(sum(t1.S),0) sumRZDService, 
+select 'zp' type, ifnull(sum(round(t1.S,1)),0) sum, null sumPay, null sumFedSoc, null sumFedNonSoc, null sumRegion, null sumWar, null sumStudy, null sumRZDPersonal, null sumRZDWork, 
+	ifnull(sum(round(t1.S,0)),0) sumRZDService, 
 	null sumService
 from ticket t1
 	join file t2 on t1.FileId=t2.FileId
